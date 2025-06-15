828c2563884165b9fb432bbf5ff33a251f5c70f6
herseo <107321911+herseo@users.noreply.github.com>
2025-06-14 23:15:01 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,21 +1,8 @@
 #!/bin/bash
 set -o errexit
 
-# Install system dependencies
-apt-get update
-apt-get install -y $(cat aptfile)
+# First upgrade core packages
+python -m pip install --upgrade pip setuptools wheel
 
-# Create virtual environment
-python -m venv venv
-source venv/bin/activate
-
-# Install packages in optimal order
-pip install --upgrade pip setuptools wheel
-
-# Install base packages first
-pip install numpy==1.24.4
-pip install pandas==2.1.4
-pip install matplotlib==3.7.2
-
-# Then install remaining packages
+# Then install requirements
 pip install -r requirements.txt
